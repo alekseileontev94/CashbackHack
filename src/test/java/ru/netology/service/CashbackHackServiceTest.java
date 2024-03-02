5442d3d17511c0a8ce813b22b055c516d60d59c5
Aleksei Leontev <aleksei_leontev94@mail.ru>
2024-03-02 21:06:29 +0500

    JUnit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -2,27 +2,28 @@ package ru.netology.service;
 
 
 
-import static org.testng.AssertJUnit.assertEquals;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 
 public class CashbackHackServiceTest {
 
 
-    @org.testng.annotations.Test
-    public void testRamin() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 1000;
-        int actual = service.remain(amount);
-        int expected = 0;
-        assertEquals(actual, expected);
-    }
-
-    @org.testng.annotations.Test
-
+    @Test
     public void testRaminWithCashback() {
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
         int actual = service.remain(amount);
         int expected = 100;
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void testRamin() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
         assertEquals(actual, expected);
     }
 }
